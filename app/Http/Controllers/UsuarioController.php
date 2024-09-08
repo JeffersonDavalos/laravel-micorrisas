1c03ec78fcce713764490cfee997954004c3cf4c
JeffersonDavalos <carreraismael766@gmail.com>
2024-09-07 19:04:26 -0500

    apis

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,30 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Usuario;  
+use App\Services\UsuarioService;
 use Illuminate\Routing\Controller; 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Exception;
-use Illuminate\Support\Facades\Log;  
+use Illuminate\Support\Facades\Log;
 
 class UsuarioController extends Controller
 {
+    protected $usuarioService;
+
+    public function __construct(UsuarioService $usuarioService)
+    {
+        $this->usuarioService = $usuarioService;
+    }
+
     /**
-     * Método para manejar la solicitud de obtener todos los usuarios.
+     * Método para manejar la solicitud de obtener usuarios filtrados por usuario y cedula.
      *
+     * @param Request $request
      * @return JsonResponse
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
         try {
-            Log::alert('entro aqui');
+            Log::alert('Entró aquí');
+            Log::alert('Datos recibidos: ' . json_encode($request->all()));
 
-            $usuarios = Usuario::all();
+            // Enviar los parámetros al servicio
+            $usuario = $request->input('usuario');
+            $cedula = $request->input('cedula');
 
-            return response()->json($usuarios, 200);
+            // Llamar al servicio para obtener los usuarios filtrados
+            $resultado = $this->usuarioService->obtenerContraseñaPorUsuarioYCedula($usuario, $cedula);
+
+            // Verificar si hubo un error en el servicio
+            if (isset($resultado['error'])) {
+                return response()->json(['error' => $resultado['error']], 500);
+            }
+
+            return response()->json($resultado, 200);
 
         } catch (Exception $e) {
-            // Manejo de errores
+            // Manejo de errores generales
             Log::error('Error al obtener usuarios: ' . $e->getMessage());
             return response()->json([
                 'message' => 'Ha ocurrido un error inesperado: ' . $e->getMessage()
--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ use App\Http\Controllers\UsuarioController;
 Route::get('/students',function(){
     return 'holi';
 });
-Route::get('/usuarios', [UsuarioController::class, 'index']);
+Route::post('/usuarios', [UsuarioController::class, 'index']);
